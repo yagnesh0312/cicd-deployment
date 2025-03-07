07b5fe5b58f83671513ca0a4e14557f99d7ee648
yagnesh0312 <yagnesh03122002@gmail.com>
2025-03-07 15:27:05 +0530

    Hello world printed

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    # return jsonify({'message': 'Hello, World!'})
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
