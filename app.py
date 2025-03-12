14a9d2512110fde30e001ad18579a6b04e64d22f
yagnesh0312 <yagnesh03122002@gmail.com>
2025-03-12 15:01:06 +0530

    finel commit 7

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello():
     # return jsonify({'message': 'Hello, World!'})
-    return "Hello Yagnesh"
+    return "Hello Yagnesh x"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
