3fb3a6bb620eaf34cd0e6b2338ba634ba5424095
yagnesh0312 <yagnesh03122002@gmail.com>
2025-03-13 16:46:09 +0530

    make it portfloio

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def hello():
-    # return jsonify({'message': 'Hello, World!'})
-    return "hello world my name is yagnesh"
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
